571499b8e6566d0201c590d62a2e2edc807b7783
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2016-02-19 12:59:56 +0100

    4/8-direction 2D movement with WASD added instead of only WS

--- Unity/shooter/Assets/Scripts/PlayerMobility.cs
+++ Unity/shooter/Assets/Scripts/PlayerMobility.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class PlayerMobility : MonoBehaviour {
 
 	public float speed;
+	private Rigidbody2D rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
 
 	void FixedUpdate()
 	{
+		//Rotate from mouse movement
 		var mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		Quaternion rot = Quaternion.LookRotation (transform.position - mousePosition, Vector3.forward);
-
 		transform.rotation = rot;
 		transform.eulerAngles = new Vector3 (0, 0, transform.eulerAngles.z);
-		GetComponent<Rigidbody2D>().angularVelocity = 0;
+		rb.angularVelocity = 0;
 
-		float input = Input.GetAxis ("Vertical");
-		GetComponent<Rigidbody2D>().AddForce (gameObject.transform.up * speed * input);
+		//WASD Movement
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+		rb.AddForce (movement * speed);
 	}
 
 }
