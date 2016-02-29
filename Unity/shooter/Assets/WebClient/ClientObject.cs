1ab591b93f170f42f3690f3fdb9577b1213aeacc
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2016-02-29 14:59:13 +0100

    Changelog: +WCF connection now works

---
+++ Unity/shooter/Assets/WebClient/ClientObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class ClientObject : MonoBehaviour
+{
+    public static wizzService service;
+
+    void Start()
+    {
+        service = new wizzService();
+
+        string mail = "mark@mark.dk";
+        string password = "daveIsAWizard";
+
+        string debug = service.ValidateUser(mail, password);
+
+        Debug.Log(debug);
+    }
+}
