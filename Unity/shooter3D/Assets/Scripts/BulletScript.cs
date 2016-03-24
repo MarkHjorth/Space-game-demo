ff68e8f809367a5ee0ddde2995c635917df701f6
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2016-03-24 16:19:20 +0100

    Changelog:

---
+++ Unity/shooter3D/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+    public ParticleSystem p;
+
+	// Use this for initialization
+	void Start () {
+	    p = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(!p.IsAlive())
+        {
+            Destroy(gameObject);
+        }
+	}
+    
+    void OnParticleCollision(GameObject other) {
+        Rigidbody body = other.GetComponent<Rigidbody>();
+        if (body) {
+            Vector3 direction = other.transform.position - transform.position;
+            direction = direction.normalized;
+            body.AddForce(direction * 5);
+        }
+    }
+}
---
+++ Unity/shooter3D/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public GameObject Bullet;
+    public Transform bulletSpawnPoint;
+    public float fireRate;
+    private float nextFire;
+    private float precision = 5.125f;
+
+	void Start ()
+	{
+        nextFire = Time.time;
+        fireRate = 0.1f;
+	}
+
+	void FixedUpdate()
+	{
+		ControlShooting();
+	}
+
+	void ControlShooting()
+	{
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            var ang = transform.rotation;
+            var accuracyMin = (ang.eulerAngles.z - precision);
+            var accuracyMax = (ang.eulerAngles.z + precision);
+            var accuracy = Random.Range(accuracyMin, accuracyMax);
+            ang.eulerAngles = new Vector3(ang.eulerAngles.x, ang.eulerAngles.y, accuracy);
+            var spawnPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            spawnPoint.y = (spawnPoint.y + 0.5f);
+            spawnPoint.x = (spawnPoint.x * 1.5f);
+            Instantiate(Bullet, bulletSpawnPoint.position, ang);
+        }
+	}
+}
