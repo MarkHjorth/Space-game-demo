8dbcba77904a7fab1eb9aa290e0933c976490dd7
Mark Hjorth Sørensen <MarkHjorthSorensen@gmail.com>
2016-04-11 11:36:35 +0200

    Changelog: +Changed layout of map +Added enemy spawners

---
+++ Unity/shooter3D/Assets/Scripts/Controller/EnemySpawnController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour {
+    GameObject[] spawnpoints;
+    public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+        spawnpoints = GameObject.FindGameObjectsWithTag("EnemySpawn");
+        SpawnEnemies(5);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	
+	}
+    
+    void SpawnEnemies(int numberOfSpawns)
+    {
+        for (int i = 0; i < numberOfSpawns; i++)
+        {
+            int spawnNumber = Random.Range(0, spawnpoints.Length);
+            var spawnpoint = spawnpoints[spawnNumber].transform.position;
+            Instantiate(enemy, spawnpoint, new Quaternion());
+        }
+    }
+}
